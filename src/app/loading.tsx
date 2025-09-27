94d2b607fe4741ecf418b7adb93d6c0721ea5f0d
Joel Júnior <joelviana250@gmail.com>
2025-09-26 21:12:00 -0300

    feat: criar componente de loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-20 h-20 border-4 border-lavanda border-t-4 border-t-verde rounded-full animate-spin"></div>
+    </div>
+  );
+}
